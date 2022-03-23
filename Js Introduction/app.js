eb750d11163a2d37c99617bd635fb73d22442370
muradorucov <orucowmurad@gmail.com>
2022-03-23 17:02:02 +0400

    app.js add

---
+++ Js Introduction/app.js
@@ -0,0 +1,31 @@
+/*Konsol əmrləri. Mesajın konsolda göstərilməsi — ən sadə əməliyyatdır.*/
+//Mesaj
+console.log('Salam Cənab!');
+//Xəbərdarlıq
+console.warn('attention');
+//Səhv,Xəta mesajı
+console.error('not found');
+//Konsolun təmizlənməsi
+console.clear();
+
+
+//Dəyişən — yaddaşda ayrılmış yerdir hansıki adı var.
+let name;
+name = 'Murad';
+document.write(name);
+
+
+//Dəyişənin dəyərinin dəyişməsi.Proqram zamanı biz dəyişənə yeni dəyər yaza bilərik.
+let nameOfRat = 'Scabbers';
+nameOfRat = 'Pettigrew';
+
+let nameOfFrog;
+nameOfFrog = 'Trevor';
+
+/*Sabitləri elan olunması
+Konstanta — dəyər dəyişilməsi mümkün olmayan dəyişəndir. 
+O const  açar sözündən istifadə etməklə təyin edilir. Belə dəyişənlər yaradılan zaman dərhal dəyər almalıdır.*/
+
+const firstName = 'Trevor';
+firstName = 'Scabbers';
+// sabiti dəyişdirərkən xəta
